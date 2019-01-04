d60aee183f6af6e2b212ebaa06cf8a49f5f1eece
Chris Shepherd <thesheps@gmail.com>
2019-01-04 21:36:01 +0000

    First commit

---
+++ __tests__/test_hello_world.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestCidrConverter(unittest.TestCase):
+    def test_cidr_block_converts_to_ip_range(self):
+        self.assertTrue(True)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cidr-gen',
+      version='0.1',
+      description='CIDR netblock generator and parser',
+      url='http://github.com/storborg/funniest',
+      author='Chris Shepherd',
+      author_email='thesheps@gmail.com',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False)
