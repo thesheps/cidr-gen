ad529c621e285af816b91bc263d9bb20bbf5f4f4
Chris Shepherd <thesheps@gmail.com>
2019-01-04 21:38:23 +0000

    Whoopsed the git url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='cidr-gen',
       version='0.1',
       description='CIDR netblock generator and parser',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/thesheps/cidr-gen',
       author='Chris Shepherd',
       author_email='thesheps@gmail.com',
       license='MIT',
